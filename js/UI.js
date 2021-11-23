4ffa62751dcd1d94ee0b643709751ef456a64e62
Carl-John <jy081320@gmail.com>
2021-11-23 09:00:56 +0800

    Displaying a Message if Form is Empty

--- js/UI.js
+++ js/UI.js
@@ -2,7 +2,25 @@ class UI {
 
 
     //Displays a Custom Message
-    printMessage() {
-        
+    printMessage(message, className) {
+        const div = document.createElement('div');
+
+        //Add the HTML
+        div.innerHTML = `
+            <div class"alert alert-dismissible alert-${className}">
+                <button type"button" class"close" data-dismiss="alert">x</button>
+                ${message}
+            </div>
+        `;
+
+        //Insert Before
+        const reference = document.querySelector('.jumbotron h1');
+        const parentNode = reference.parentElement;
+        parentNode.insertBefore(div, reference);
+
+        //Remove After 3sec
+        setTimeout(() => {
+            document.querySelector('.alert').remove();
+        }, 3000);
     }
 }
