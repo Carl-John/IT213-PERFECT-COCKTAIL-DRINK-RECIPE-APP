bd2901b16c2c0d266773eea9c6adba4dbd61c662
Carl-John <jy081320@gmail.com>
2021-11-29 09:25:16 +0800

    Adding the Information from the REST API to the Template

--- js/UI.js
+++ js/UI.js
@@ -15,27 +15,27 @@ class UI {
             resultsDiv.innerHTML += `
                 <div class"col-md-6">
                     <div class="card ,y-3">
-                        <img class="card-img-top" src="" alt="">
+                        <img class="card-img-top" src="http://${drink.strDrinkThumb}" alt="${drink.strDrink}">
                         
                         <div class="card-body">
-                            <h2 class="card-title text-center"></h2>
+                            <h2 class="card-title text-center">${drink.strDrink}</h2>
                             <p class="card-text font-weight-bold">Instructions: </p>
                             <p class="card-text">
-
+                                ${drink.strInstructions}
                             </p>
                             <p class="card-text">
                                 <ul class="list-group">
                                     <li class="list-group-item alert alert-danger">Ingredients</li>
-
+                                    ${this.displayIngredients(drink)}
                                 </ul>
                             </p>
                             <p class="card-text font-weight-bold">Extra Information:</p>
                             <p class=""card-text>
                                 <span class="badge badge-pill badge-success">
-
+                                    ${drink.strAlcoholic}
                                 </span>
                                 <span class="badge badge-pill badge-warning">
-                                    Category: 
+                                    Category: ${drink.strCategory}
                                 </span>
                             </p>
                         </div>
@@ -43,6 +43,33 @@ class UI {
                 </div>
             `;
         })
+    }
+
+    //Prints the Ingredients and Measurements
+    displayIngredients(drink) {
+        //console.log(drink);
+
+        let ingredients = [];
+        for(let i = 1; 1 < 16; i++) {
+            const ingredientMeasure = {};
+            if( drink[`strIngredient${i}`] !== '') {
+                ingredientMeasure.ingredient = drink[`strIngredient${i}`];
+                ingredientMeasure.measure = drink[`strMeasure${i}`];
+                ingredients.push(ingredientMeasure);
+            }
+            
+        }
+
+        //console.log(ingredients);
+        //Build the template
+
+         let ingredientsTemplate = '';
+         ingredients.forEach(ingredient => {
+             ingredientsTemplate += `
+                <li class="list-group-item">${ingredient.ingredient} - ${ingredient.measure}</li>
+             `;
+         });
+         return ingredientsTemplate;
     }
 
     //Displays a Custom Message
